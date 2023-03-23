4e79e9dda4af8efa758d9464c07beb5a448bf761
mymoon69 <88487704+mymoon69@users.noreply.github.com>
2023-03-24 01:54:55 +0700

    Add files via upload

--- youblog/routes/index.js
+++ youblog/routes/index.js
@@ -4,10 +4,21 @@ const router = express.Router()
 // ดึงข้อมูล json มาเก็บไว้ในตัวแปร
 const article = require('../article-db')
 
-router.get('/', function(req, res, next) {
-    var data = { 
-        article: article } //เอาข้อมูลใน json ใส่
-    res.render('index', data)//render html index ชื่อไฟล์
+router.get('/', function(req, res) {
+    var data = {article : article}
+    var search = req.query.search
+    if(search == undefined){
+        var data = { 
+            article: article } //เอาข้อมูลใน json ใส่ //not filter
+            res.render('index', data)//render html index ชื่อไฟล์
+    }else{
+        var data = { article: article.filter(value => {
+            return value.title.toLowerCase().includes(search.toLowerCase())
+        })}
+        res.render('index', data)//render html index ชื่อไฟล์
+    }
+    console.log(data)
+    console.log(search) //not search = underfined
 })
 
 module.exports = router
