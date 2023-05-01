d1cfde7d8782f75cea5ca09df77056d0bb827575
mymoon69 <64070105@it.kmitl.ac.th>
2023-05-02 00:52:38 +0700

    ww

--- WEEK11-TUTORIAL-main/backend/config.js
+++ WEEK11-TUTORIAL-main/backend/config.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: '1234',
-  database: 'webpro',
+  password: 'sasikan',
+  database: 'week10',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
